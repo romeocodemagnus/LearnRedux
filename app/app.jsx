41c3cb35590821ad14bcf937c6c10f5097c2d0e3
romeocodemagnus <rflauta@codemagnus.com>
2016-06-23 12:44:05 +0800

    Add subscribe and devtools

--- app/app.jsx
+++ app/app.jsx
@@ -8,9 +8,11 @@ $(document).foundation();
 // App css
 require('style!css!sass!applicationStyles')
 
+/*
 ReactDOM.render(
   <p>Boilerplate 3 Project</p>,
   document.getElementById('app')
 );
+*/
 
 require('./redux-example.jsx');
--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,7 +1,62 @@
 var redux = require('redux');
 
-console.log('Starting redux example');
 
-function add (a, b) {
-	return a + b;
+var stateDefault = {
+	name: '',
+	searchText: '',
+	showCompleted: false,
+	todos: []
 }
+
+var reducer = (state = stateDefault, action) => {
+	switch (action.type){
+		case 'CHANGE_SEARCH_TEXT':
+			return {
+				...state,
+				searchText: action.searchText
+			};
+		case 'CHANGE_NAME':
+			return {
+				...state,
+				name: action.name
+			};
+		default: 
+			return state;
+	}
+};
+
+var store = redux.createStore(reducer, redux.compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+var unsubscribe = store.subscribe( () => {
+	var state = store.getState();
+	console.log('current state: ', state);
+
+	document.getElementById('app').innerHTML = state.name;
+});
+
+
+/*
+var action = {
+	type: 'CHANGE_SEARCH_TEXT',
+	searchText: 'Apple'
+}
+
+store.dispatch(action);
+*/
+
+var action = {
+	type: 'CHANGE_NAME',
+	name: 'Meo'
+}
+store.dispatch(action);
+
+// unsubscribe();
+
+var action = {
+	type: 'CHANGE_NAME',
+	name: 'Sedh'
+}
+store.dispatch(action);
+
